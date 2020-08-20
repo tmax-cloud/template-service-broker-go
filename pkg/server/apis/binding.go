02d8e8c6eef8cfaf9ac4ded5f187f7009d530d32
= <=>
2020-08-20 16:13:05 +0900

    [feat] add binding api

---
+++ pkg/server/apis/binding.go
@@ -0,0 +1,131 @@
+package apis
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/jwkim1993/template-service-broker/internal"
+	"github.com/jwkim1993/template-service-broker/pkg/server/schemas"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var logBind = logf.Log.WithName("binding")
+
+func BindingServiceInstance(w http.ResponseWriter, r *http.Request) {
+	var m schemas.ServiceBindingRequest
+
+	//set reponse
+	response := &schemas.ServiceBindingResponse{}
+	response.Credentials = make(map[string]interface{})
+	w.Header().Set("Content-Type", "application/json")
+
+	//get request body
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		logBind.Error(err, "error occurs while decoding service binding body")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//get url param
+	vars := mux.Vars(r)
+
+	//get templateinstance name & namespace
+	instanceName := m.ServiceId + "." + m.PlanId + "." + vars["instance_id"]
+	instanceNameSpace, err := internal.Namespace()
+	if err != nil {
+		logBind.Error(err, "cannot get namespace")
+	}
+
+	//add template & template instance schema
+	s := scheme.Scheme
+	internal.AddKnownTypes(s)
+	SchemeBuilder := runtime.NewSchemeBuilder()
+	if err := SchemeBuilder.AddToScheme(s); err != nil {
+		logBind.Error(err, "cannot add Template/Templateinstance scheme")
+		return
+	}
+
+	// connect k8s client
+	c, err := internal.Client(client.Options{Scheme: s})
+	if err != nil {
+		logBind.Error(err, "cannot connect k8s api server")
+		return
+	}
+
+	// get templateinstance info
+	templateInstance, err := internal.GetTemplateInstance(c, types.NamespacedName{Name: instanceName, Namespace: instanceNameSpace})
+	if err != nil {
+		logBind.Error(err, "cannot get templateinstance info")
+		return
+	}
+
+	// parse object info in template info
+	for _, object := range templateInstance.Spec.Template.Objects {
+		var raw map[string]interface{}
+		if err := json.Unmarshal(object.Raw, &raw); err != nil {
+			logBind.Error(err, "cannot get object info")
+		}
+
+		//get kind, namespace, name of object
+		kind := raw["kind"].(string)
+		name := raw["metadata"].(map[string]interface{})["name"].(string)
+		namespace := instanceNameSpace
+
+		if strings.Compare(kind, "Service") == 0 {
+			//set endpoint in case of service
+			service := &corev1.Service{}
+			if err := c.Get(context.TODO(), types.NamespacedName{Namespace: namespace, Name: name}, service); err != nil {
+				logBind.Error(err, "error occurs while getting service info")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if service.Spec.Type == corev1.ServiceTypeLoadBalancer {
+				ports := []string{}
+				for _, port := range service.Spec.Ports {
+					ports = append(ports, strconv.FormatInt(int64(port.Port), 10))
+				}
+				for _, ingress := range service.Status.LoadBalancer.Ingress {
+					endpoint := schemas.ServiceBindingEndpoint{
+						Host:  ingress.IP,
+						Ports: ports,
+					}
+					response.Endpoints = append(response.Endpoints, endpoint)
+				}
+			}
+
+		} else if strings.Compare(kind, "Secret") == 0 {
+			//set credentials in case of secret
+			secret := &corev1.Secret{}
+			if err := c.Get(context.TODO(), types.NamespacedName{Namespace: namespace, Name: name}, secret); err != nil {
+				logBind.Error(err, "error occurs while getting secret info")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			for key, val := range secret.Data {
+				response.Credentials[key] = string(val)
+			}
+		}
+	}
+	//set credential if not empty Endpoints
+	if len(response.Endpoints) != 0 {
+		response.Credentials["endpoints"] = response.Endpoints
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func UnBindingServiceInstance(w http.ResponseWriter, r *http.Request) {
+	response := &schemas.AsyncOperation{}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
